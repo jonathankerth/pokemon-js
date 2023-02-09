6b6e96a612c1d8652f38d11de280a25c3f5e96d1
jonathankerth <jonathanpkerth@gmail.com>
2023-02-09 15:57:42 -0800

    added pokemonList in js and 3 starting pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
-alert("Hello world");
-let favoriteFood = "pizza";
+let pokemonList = [
+	{ name: "bulbasaur", heigh: ".7", types: ["grass", "poison"] },
+	{ name: "squitle", height: ".5", types: ["water", "monster"] },
+	{ name: "ekans", height: "2", types: ["ground", "psychic"] },
+];
