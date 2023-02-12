316dd15671683b40c51fe4c9f44e2ebf0a4bedc1
jonathankerth <jonathanpkerth@gmail.com>
2023-02-11 17:50:32 -0800

    added conditional loop for pokemon  and css file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 let pokemonList = [
-	{ name: "bulbasaur", heigh: ".7", types: ["grass", "poison"] },
+	{ name: "bulbasaur", height: ".7", types: ["grass", "poison"] },
 	{ name: "squitle", height: ".5", types: ["water", "monster"] },
 	{ name: "ekans", height: "2", types: ["ground", "psychic"] },
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+	let currentPokemon = pokemonList[i];
+	document.write(currentPokemon.name + " " + currentPokemon.height);
+	if (currentPokemon.height > 1) {
+		document.write("Wow! That's big!");
+	}
+}
