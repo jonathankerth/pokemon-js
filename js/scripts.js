80fdb2a36333dbf0c2e3b229abc44b6361e0bad3
jonathankerth <jonathanpkerth@gmail.com>
2023-02-16 16:06:22 -0800

    corrected errors in forEach loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,31 @@
 let pokemonRepository = (function () {
 	let pokemonList = [
-		{ name: "bulbasaur", height: 0.7, types: ["grass", "poison"] },
-		{ name: "squirtle", height: 0.5, types: ["water", "monster"] },
-		{ name: "ekans", height: 2, types: ["ground", "psychic"] },
+		{ name: "bulbasaur", height: ".7", types: ["grass", "poison"] },
+		{ name: "squitle", height: ".5", types: ["water", "monster"] },
+		{ name: "ekans", height: "2", types: ["ground", "psychic"] },
 	];
+	function getAll() {
+		return pokemonList;
+	}
 
 	function add(pokemon) {
 		pokemonList.push(pokemon);
 	}
-
-	function getAll() {
-		pokemonList.forEach(function (pokemon) {
-			if (pokemon.height > 1) {
-				console.log(
-					pokemon.name +
-						" is " +
-						pokemon.height +
-						"M's tall. Wow, that's a big pokemon!"
-				);
-			} else {
-				console.log(
-					pokemon.name + " is " + pokemon.height + "M's tall"
-				);
-			}
-		});
-	}
-
 	return {
-		add: add,
 		getAll: getAll,
+		add: add,
 	};
 })();
-console.log(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(function (pokemon) {
+	if (pokemon.height > 1) {
+		document.write(
+			pokemon.name +
+				"is (" +
+				pokemon.height +
+				")M's tall. Wow, that's a big pokemon!"
+		);
+	} else {
+		document.write(pokemon.name + " is (" + pokemon.height + ")M's tall");
+	}
+	document.write("<br>");
+});
